b239f53b958aa5fb4092d5dc8cdf6a1bbbc99027
jivison <john@ivison.org>
2020-10-15 17:53:11 -0700

    Fix bug in cralias where error didn't quit after

--- cralias/cralias.py
+++ cralias/cralias.py
@@ -133,6 +133,7 @@ if __name__ == "__main__":
         if len(sys.argv) < 3:
             print(
                 f"Please specifiy an alias and something to alias (eg. {COLORS['yellow']}cralias k kubectl{COLORS['reset']})!")
+            quit()
 
         cralias.add(sys.argv[1], *sys.argv[2:])
 
